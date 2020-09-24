64fb994bcdccc6d5f0dd98d2181a2e5ed1ddd062
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2020-09-24 23:54:42 +0600

    added ProductImage model and collect static files

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductImage
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class ProductAdmin(admin.ModelAdmin):
         model = Product
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductImage)
--- products/models.py
+++ products/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length = 120)
     description = models.TextField(null= True, blank= True)
     price = models.IntegerField()
+    sale_price = models.IntegerField(default=100)
     slug = models.SlugField()
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
@@ -15,3 +16,14 @@ class Product(models.Model):
 
     def get_price(self):
         return self.price
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete= models.CASCADE )
+    image = models.ImageField(upload_to = 'products/images/')
+    featured = models.BooleanField(default= False)
+    thumbnail = models.BooleanField(default= False)
+    active = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.product.title
