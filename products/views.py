d58e3c5d645c22fc8fa9c1f88bffe58adcf60794
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2020-09-25 02:01:36 +0600

    Display products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Product, ProductImage
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    products = Product.objects.all()
+    return render(request, 'products/index.html',{
+        'products': products
+    })
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'products/all_product.html',{
+        'products': products
+    })
