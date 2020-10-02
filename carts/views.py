314792826adc91acb1ea8f89bb47f755ffd91ea3
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2020-10-02 11:45:48 +0600

    add session to cart

--- carts/views.py
+++ carts/views.py
@@ -4,13 +4,27 @@ from products.models import Product
 from django.urls import reverse
 # Create your views here.
 def cart(request):
-    carts = Cart.objects.all()[0]
-    return render(request, 'carts/index.html',{
-        'carts': carts
-    })
+    try:
+        the_id = request.session['cart_id']
+    except:
+        the_id = None
+    if the_id:
+        carts = Cart.objects.get(id = the_id)
+        return render(request,'carts/index.html',{'carts':carts})
+    else:
+        empty_message = 'Your cart is empty, please keep shopping!'
+        return render(request,'carts/index.html',{'empty': True,'empty_message': empty_message,})
 
 def update_cart(request, slug):
-    cart = Cart.objects.all()[0]
+    request.session.set_expiry(120000)  #after 120000 secs cart will be empty
+    try:
+        the_id = request.session['cart_id']
+    except:
+        new_cart = Cart()
+        new_cart.save()
+        request.session['cart_id'] = new_cart.id
+        the_id = new_cart.id
+    cart = Cart.objects.get(id = the_id)
     try:
         product = Product.objects.get(slug = slug)
     except Product.DoesNotExist:
@@ -23,6 +37,7 @@ def update_cart(request, slug):
     new_total = 0.00
     for item in cart.products.all():
         new_total += float(item.price)
+    request.session['items_total'] = cart.products.count()
     cart.total = new_total
     cart.save()
     return HttpResponseRedirect(reverse('cart'))
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, Http404
 from .models import Product, ProductImage
 
+
 # Create your views here.
 def search(request):
     try:
@@ -8,34 +9,37 @@ def search(request):
     except:
         k = None
     if k:
-        products = Product.objects.filter(title__icontains = k)
-        return render(request, 'products/results.html',{
+        products = Product.objects.filter(title__icontains=k)
+        return render(request, 'products/results.html', {
             'query': k,
-            'products':products,
+            'products': products,
         })
     else:
         return render(request, 'products/results.html')
 
+
 def index(request):
     products = Product.objects.all()
-    return render(request, 'products/index.html',{
+    return render(request, 'products/index.html', {
         'products': products
     })
+
 
 def all_products(request):
     products = Product.objects.all()
-    return render(request, 'products/all_product.html',{
+    return render(request, 'products/all_product.html', {
         'products': products
     })
 
+
 def single(request, slug):
     try:
-        product = Product.objects.get(slug = slug)
-        images = ProductImage.objects.filter(product = product)
-        return render(request, 'products/single.html',{
+        product = Product.objects.get(slug=slug)
+        images = ProductImage.objects.filter(product=product)
+        return render(request, 'products/single.html', {
             'product': product,
             'images': images
-    })
+        })
+
     except:
         raise Http404
-
