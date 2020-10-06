5ab67d6d31ea0daf642d1d7ace72eb5fbe1c34d5
Md. Rakibul Islam <mdrakibul.islam8001@gmail.com>
2020-10-06 09:20:04 +0600

    modified templates

--- carts/admin.py
+++ carts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart, CartItem
 # Register your models here.
 class CartAdmin(admin.ModelAdmin):
     class Meta:
         model = Cart
 
 admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem)
